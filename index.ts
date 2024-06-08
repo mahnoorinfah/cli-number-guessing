2aad0ea4a4d539864e47103bf3e222b285d328db
MahnoorShafiq <161619478+mahnoorinfah@users.noreply.github.com>
2024-06-08 11:01:02 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,19 @@
+import inquirer from "inquirer";
+
+const guessNumber = Math.floor(Math.random() * 10 + 1);
+
+
+const ans = await inquirer.prompt([{
+    name : "userGuessednumber",
+    type : "number",
+    message : "Please guess any number between 1-10 : ",
+
+},
+]);
+if (ans.userGuessednumber === guessNumber) {
+    console.log("Great! You guess correct number. ");
+}
+else{
+    console.log("You guessed wrong number");
+    
+}
